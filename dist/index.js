117f8da7427c937b8496bc0a2c50f23af56b6149
Mike Krieger <m@mikeyk.co>
2021-11-02 20:05:55 -0700

    No os available

--- dist/index.js
+++ dist/index.js
@@ -2317,7 +2317,7 @@ class SwiftFormatOfficial {
 		const lintResult = initLintResult();
 
 		const matches = output.stderr.matchAll(PARSE_REGEX);
-    process.stdout.write("matches " + matches + os.EOL);
+    process.stdout.write("matches " + matches + "\n")
 		for (const match of matches) {
 			const [_line, pathFull, line, _column, _level, message] = match;
 			const path = pathFull.substring(dir.length + 1);
